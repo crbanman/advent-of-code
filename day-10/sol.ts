d36d5d046e65526a6446bab42f3d80734a7ebd12
Cody Banman <crbanman@gmail.com>
2022-12-10 13:03:10 -0800

    ran code formatter

--- day-10/sol.ts
+++ day-10/sol.ts
@@ -1,4 +1,6 @@
-const input = Deno.readTextFileSync("input.txt").split(/\n/g).map((val) => val.split(' '))
+const input = Deno.readTextFileSync("input.txt").split(/\n/g).map((val) =>
+  val.split(" ")
+);
 
 let cycles = 1;
 let register = 1;
@@ -10,36 +12,35 @@ const crt: string[] = [];
 const checkStrength = () => {
   if (cycles === 20 || (cycles - 20) % 40 === 0) {
     interestingSignals.push(register * cycles);
-    interestingSignalsSum += register * cycles
+    interestingSignalsSum += register * cycles;
   }
-}
+};
 
 const render = () => {
-  const pixel = (cycles - 1) % 40
-  crt.push((pixel >= (register - 1) && pixel <= (register + 1 )) ? '#' : '.')
-}
-
+  const pixel = (cycles - 1) % 40;
+  crt.push((pixel >= (register - 1) && pixel <= (register + 1)) ? "#" : ".");
+};
 
 input.forEach(([op, param]) => {
-  if (op === '') {
+  if (op === "") {
     return;
   }
   render();
   ++cycles;
   checkStrength();
-  if (op === 'noop') {
+  if (op === "noop") {
     return;
   }
   render();
   ++cycles;
-  register += Number(param)
-  checkStrength()
-})
+  register += Number(param);
+  checkStrength();
+});
 
 let crtOutput = "";
 crt.forEach((val, index) => {
-  crtOutput += index % width === 0 ? `\n${val}` : val
-})
+  crtOutput += index % width === 0 ? `\n${val}` : val;
+});
 
-console.log("Sum of interesting signal strengths:", interestingSignalsSum)
-console.log(crtOutput)
+console.log("Sum of interesting signal strengths:", interestingSignalsSum);
+console.log(crtOutput);
